d376d3f022b5736f86276b753b012dd185b09c28
EvanBeugnot <evan.beugnot@iut-dijon.u-bourgogne.fr>
2023-10-23 16:29:34 +0200

    TP5 - 3.2 : Implémentation de la fonction Index

--- controllers/MainController.php
+++ controllers/MainController.php
@@ -2,4 +2,9 @@
 
     require_once("View.php");
 
+    public function Index() : void {
+        $indexView = new View('Index');
+        $indexView->generer(['nomDresseur' => "Red"]);
+    }
+    
 ?>
